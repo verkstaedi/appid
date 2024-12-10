8c612a2dc93465d67f14d377912ce912c5366dd8
isak <isakka21@tskoli.is>
2024-12-10 01:19:18 +0000

    Implement user authentication with login and logout functionality, update navigation links, and add logout template

--- vefsida/isark_website/main/urls.py
+++ vefsida/isark_website/main/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.login_view, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', views.register_view, name='register'),
     path('hafa-samband/', views.contact, name='contact'),
-    path('accounts/', include('allauth.urls')),
+    path('', views.index, name='index'),
 ]
