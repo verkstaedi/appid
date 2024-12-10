3712df9edb4b17f77f0fe2ec29ee47d09cec0ed0
isak <isakka21@tskoli.is>
2024-12-10 05:26:29 +0000

    Change database configuration from SQLite to MySQL with updated credentials

--- vefsida/isark_website/isark_website/settings.py
+++ vefsida/isark_website/isark_website/settings.py
@@ -96,14 +96,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'isark_website.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dataworkshop',
+        'USER': 'root',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
